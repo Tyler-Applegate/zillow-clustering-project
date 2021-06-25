7a84144c47c4591115aad613c1a8fe483b02e744
Tyler-Applegate <tyler.applegate@gmail.com>
2021-06-25 13:03:05 -0500

    updated functions

--- explore.py
+++ explore.py
@@ -25,3 +25,26 @@ def plot_variable_dist(df, figsize = (3,2)):
             plt.show()
             print(f'Number of Nulls: {df[col].isnull().sum()}')
             print('--------------------------------------')
+            
+            
+def zillow_dummies(train, validate, test, dummy):
+    '''
+    This function takes in train, validate, and test dataframes, as well as a target variable,
+    (target is a string) and creates dummies on each dataframe. Then returns the updated train,
+    validate, and test DataFrames.
+    '''
+
+#     Create dummy variables of the target variable for the train DataFrame.
+    dummy_train = pd.get_dummies(train[[dummy]], dummy_na=False, drop_first=True)
+# let's put it all together...
+    train = pd.concat([train, dummy_train], axis=1)
+#     Create dummy variables of the target variable for the validate DataFrame.
+    dummy_validate = pd.get_dummies(validate[[dummy]], dummy_na=False, drop_first=True)
+# let's put it all together...
+    validate = pd.concat([validate, dummy_validate], axis=1)
+#     Create dummy variables of the target variable for the test DataFrame.
+    dummy_test = pd.get_dummies(test[[dummy]], dummy_na=False, drop_first=True)
+# let's put it all together...
+    test = pd.concat([test, dummy_test], axis=1)
+    
+    return train, validate, test
